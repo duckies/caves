e85741df8ffeef013076e5afd93ef4717d063939
Quynh Nguyen <39203622+quynhn1404@users.noreply.github.com>
2020-12-09 12:45:29 -0500

    add collider dialog

---
+++ Assets/Scripts/Dialog/DialogBlock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogBlock : MonoBehaviour
+{
+    //[SerializeField] private GameObject tree = null;
+    //[SerializeField] private Dialog allArea = null;
+    [SerializeField] private Dialog forestArea = null;
+    [SerializeField] private Dialog lavaArea = null;
+
+    Collider2D collide;
+    //private int treeState;
+    void Start()
+    {
+        //Fetch the GameObject's Collider (make sure it has a Collider component)
+        collide = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        if (collide.enabled)
+        {
+            Debug.Log(name + " activated");
+            //DialogManager.instance.StartDialog(allArea);
+
+            if(name == "ForestBarrier")
+                DialogManager.instance.StartDialog(forestArea);
+            else
+                DialogManager.instance.StartDialog(lavaArea);
+        }
+    }
+}
